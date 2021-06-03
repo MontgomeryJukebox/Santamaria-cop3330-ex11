8b1bc44d92be0f593bd9ccbd5f12ef279fc42789
David Santamaria <davidsantamaria1245@gmail.com>
2021-06-02 22:04:34 -0400

    Upload files

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final double USD_EXCHANGE_RATE = 100.00278;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How many euros are you exchanging?");
+        int euros = in.nextInt();
+        System.out.print("What is the exchange rate?");
+        double rate = in.nextDouble();
+        double amount_to = (euros * rate) / USD_EXCHANGE_RATE;
+        System.out.printf("%d euros at an exchange rate of %.2f is\n", euros, rate);
+        System.out.printf("%.2f U.S. dollars.", amount_to);
+    }
+}
